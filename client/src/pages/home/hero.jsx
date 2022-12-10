7366cdaa60b19464d391156efd8446d02b2a61fa
Eboreime Rhoda <rhodaivioje@gmail.com>
2022-12-10 13:20:44 +0100

    adjusted hero padding

--- client/src/pages/home/hero.jsx
+++ client/src/pages/home/hero.jsx
@@ -5,7 +5,7 @@ import { BsArrowRight } from 'react-icons/bs'
 
 const Hero = () => (
     <section className={`bg-primary md:h-[90vh] flex-col flex justify-center items-center md:flex-row `}>
-        <div className='w-full mt-10 md:mt-0 md:w-[50%] px-10 ex:px-0 ex:ml-[74px] font-poppins 
+        <div className='w-full mt-10 md:mt-0 md:w-[50%] px-7 ex:px-0 ex:ml-[74px] font-poppins 
         text-dimWhite pr-2 md:mb-0 mb-5' >
             <h1 className='text-[35px] ex:text-[45px] font-[500]'>
                 Upload and view profiles all in {' '}
