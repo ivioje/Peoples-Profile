4fbad93f4a9fad9397845f46b8b552d92e53a6f1
Eboreime Rhoda <rhodaivioje@gmail.com>
2022-12-18 20:55:58 +0100

    --

--- client/src/routes/profiles/cards.jsx
+++ client/src/routes/profiles/cards.jsx
@@ -25,7 +25,7 @@ const Cards = () => {
                             style={{ 'backgroundImage': `url(${items.photo})` }}
                         />
                         <button className='absolute xs:top-[110px] right-0 bg-white w-[30px] xs:w-[40px] h-[30px] xs:h-[40px]
-                        flex items-center justify-center text-[14px] xs:text-[19px] rounded-full mr-1 ss:mr-2 shadow text-primary'
+                        flex items-center justify-center text-[14px] xs:text-[19px] rounded-full mr-1 ss:mr-2 shadow-lg outline-none xs:shadow text-primary'
                             onClick={() => setFill(items.id)}>
                             {fill === items.id ? <BsHeartFill /> : <BsHeart />}
                         </button>
