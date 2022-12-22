4a51dbd40105937710236b8267b7ddb83b3add3c
Eboreime Rhoda <rhodaivioje@gmail.com>
2022-12-22 22:35:57 +0100

    login component

--- client/src/ErrorPage.jsx
+++ client/src/ErrorPage.jsx
@@ -10,9 +10,9 @@ export default function ErrorPage() {
         <div className={`${styles.flexCenter} flex-col h-[100vh] text-center`}>
             <h1>Oops!</h1>
             <p>
-                <i>{error.statusText || error.message}</i>
+                <i className='text-[20px] font-bold'>{error.statusText || error.message}</i>
             </p>
-            <p className='text-[25px] font-bold'>Sorry, an unexpected error has occurred.<br /> Return
+            <p className='text-[22px]'>Sorry, an unexpected error has occurred.<br /> Return
                 <Link to='/' className="text-gradient border-b-2 border-dotted border-gray-500 m-2 ">
                     {' '}  Home
                 </Link>
--- client/src/routes/login/Login.jsx
+++ client/src/routes/login/Login.jsx
@@ -4,29 +4,35 @@ import styles from '../../style'
 
 const Login = () => {
     return (
-        <section className={`font-poppins div-overlay ${styles.flexCenter} text-text_color`}>
+        <section className={`font-poppins div-overlay ${styles.flexCenter} text-text_color p-1`}>
             <div className={`bg-white ${styles.flexBtw} flex-col p-10 my-10 md:my-16 sm:my-12 md:w-[590px] sm:w-[60%] w-[90%] rounded-[34px] `}>
-                <h2 className='font-[600] p-1'>Welcome back!</h2>
+                <h2 className='font-[600] p-1 text-[22px]'>Welcome back!</h2>
                 <p>Log in to continue</p>
 
                 <form className={`${styles.flexBtw} flex-col mt-12 w-[90%]`}>
                     <input name='email' type='text' placeholder='Email address or username'
-                    className='h-9 p-2 mb-8 placeholder:text-text_color placeholder:font-[200] bg-slate-50 rounded w-full border border-gray-100' />
+                        className='h-9 p-2 mb-8 placeholder:text-text_color placeholder:font-[100] bg-slate-50 rounded w-full border border-gray-100' />
                     <input name='password' type='password' placeholder='Password'
-                    className='h-9 p-2 mb-8 placeholder:text-text_color placeholder:font-[200] bg-slate-50 rounded w-full border border-gray-100' />
-                    <div>
-                        <label>
+                        className='h-9 p-2 mb-6 placeholder:text-text_color placeholder:font-[100] bg-slate-50 rounded w-full border border-gray-100' />
+                    <div className={`${styles.flexBtw} w-full text-[15px]`}>
+                        <label className='cursor-pointer'>
                             <input name='rememberMe' type='checkbox' />
                             {' '} Remember me
                         </label>
-                        <p>Forgot password?</p>
+                        <p className='text-primary cursor-pointer'>Forgot password?</p>
                     </div>
-                    <button>Log in</button>
+                    <Link to='/dashboard' className='w-full p-2 mt-10 mb-6 bg-primary rounded text-dimWhite bg-opacity-95  hover:bg-opacity-100 text-center '>
+                            Log in
+                    </Link>
                     <p>Or</p>
-                    <button>Continue with Google</button>
+                    <div className={`${styles.flexBtw} border p-1 my-6`}>
+                        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2048px-Google_%22G%22_Logo.svg.png'
+                            alt='' className='w-[25px] h-[25px] mx-3 ' />
+                        <button className=' p-1  rounded-[4px]'>Continue with Google</button>
+                    </div>
                     <p>Don't have an account?
-                        <Link to='/signin'>
-                            Sign in
+                        <Link to='/signin' className='text-primary'>
+                            {' '} Sign in
                         </Link>
                     </p>
 
