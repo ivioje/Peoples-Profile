85982782835e4eae37563a68b1730432da65669c
Eboreime Rhoda <rhodaivioje@gmail.com>
2022-12-25 22:52:26 +0100

    --

--- client/src/components/navbar/GetStarted.jsx
+++ client/src/components/navbar/GetStarted.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const GetStarted = () => (
     <div className='pl-3'>
         <Link to='/signup'>
-    <button className='w-[157px] h-[42px] bg-dimWhite text-primary rounded-lg font-medium font-poppins block'>
+    <button className='w-[157px] h-[42px] bg-dimWhite text-primary rounded-lg font-medium font-poppins'>
         Get Started
     </button>
     </Link>
