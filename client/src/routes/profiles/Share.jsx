574c02bac3bd4854b5ba58d75d5c03007852dcd1
Eboreime Rhoda <rhodaivioje@gmail.com>
2022-12-29 10:59:55 +0100

    share modal

--- client/src/routes/profiles/Share.jsx
+++ client/src/routes/profiles/Share.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { BsFacebook, BsGithub, BsLinkedin, BsTwitter, BsX } from 'react-icons/bs'
+import styles from '../../style'
 
 const Share = () => {
   return (
-    <section className='absolute transform xs:translate-x-[50px] xs:translate-y-48
-    border py-4 px-10 translate-y-36 translate-x-[30px]'>
-        Share
+    <section className='absolute border-4 border-gray-100 pt-6 px-2 xs:p-4 w-auto h-auto bottom-[-20px] right-0 bg-white shadow'>
+      <button className='absolute top-0 right-0 text-[26px]'>
+        <BsX />
+      </button>
+      <div className={`${styles.flexBtw} flex-wrap text-xl text-primary share mt-1`}>
+        <a href='#'><BsTwitter /></a>
+        <a href='#'><BsGithub /></a>
+        <a href='#'><BsLinkedin /></a>
+        <a href='#'><BsFacebook /></a>
+      </div>
     </section>
   )
 }
