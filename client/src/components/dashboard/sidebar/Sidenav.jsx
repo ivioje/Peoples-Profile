e29aed1a718d2751e4b39640c5b088b438cee28f
Eboreime Rhoda <rhodaivioje@gmail.com>
2023-01-11 18:58:38 +0100

    dashboard routing

--- client/src/components/dashboard/sidebar/Sidenav.jsx
+++ client/src/components/dashboard/sidebar/Sidenav.jsx
@@ -12,8 +12,8 @@ const DashboardNav = () => {
         background: 'rgba(51, 66, 87, 0.26)'
     }
     return (
-        <div className=' flex'>
-            <section className={` h-full w-full ss:w-[350px] ${styles.flexBtw} flex-wrap relative`}>
+        <div className=' xs:flex'>
+            <section className={` h-full w-full ss:w-[350px] ${styles.flexBtw} flex-wrap relative font-poppins`}>
                 <div className={` font-poppins ${styles.flexCol} `}>
                     <ul className='list-none flex hide flex-col justify-center items-start flex-1 pt-20 pl-20 pr-2 pb-2'>
                         {
@@ -34,11 +34,13 @@ const DashboardNav = () => {
                             <BsChevronRight className='' />}
                     </span>
 
-                    <div className={`${toggle ? 'flex' : 'hidden'} p-6 dashBSidebar h-full flex-col`}>
-                        <ul className={`flex justify-start absolute h-[500px] w-[100vw] left-0 flex-col z-1 pt-8 pb-2 px-1`}>
+                    <div className={`${toggle ? 'flex' : 'hidden'} dashBSidebar h-full flex-col items-center justify-between `}>
+                        <ul className={`flex justify-start absolute h-fit w-[100vw] left-0 flex-col z-1 pt-14 pb-2 px-1 bg-secondary`}>
                             {
                                 sidenavItems.map((item => (
-                                    <NavLink to={`/${item.id}`} key={item.id} className='py-2'>
+                                    <NavLink to={`${item.id}`} key={item.id} className='py-2 w-full pl-1 rounded-[3px]'
+                                        style={({ isActive }) => isActive ? activeStyle : undefined}
+                                        onClick={() => setToggle(false)}>
                                         {item.title}
                                     </NavLink>
                                 )))
@@ -47,9 +49,9 @@ const DashboardNav = () => {
                     </div>
                 </div>
             </section>
-                <div id='detail'>
-                    <Outlet />
-                </div>
+            <div id='detail'>
+                <Outlet />
+            </div>
 
         </div>
     )
