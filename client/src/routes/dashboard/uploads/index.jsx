eb92b163725badb230384b29b7fad8ec09440f11
Eboreime Rhoda <rhodaivioje@gmail.com>
2023-01-19 14:23:58 +0100

    uploaded profiles layout

--- client/src/routes/dashboard/uploads/index.jsx
+++ client/src/routes/dashboard/uploads/index.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import ProfileUploads from './UploadedProfiles'
+import React from 'react';
+import ProfileUploads from './UploadedProfiles';
+import uploadIcon from '../../../assets/cloud-upload.svg'
 
 const UploadedProfiles = () => {
   return (
     <section className='font-poppins m-2 xs:p-2 flex flex-col xs:items-start items-center'>
       <div className='sm:mt-10 mt-1 xs:p-3  '>
-        <p className='w-full'>
+        <div className='flex items-center'>
+          <img src={uploadIcon} alt="icon" />
+        <p className='w-full mx-1'>
           Your uploaded profiles
         </p>
+        </div>
         <hr />
         <p className='my-2'>
           Here, you'll find all the profiles you've created
