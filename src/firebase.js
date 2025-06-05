514db021ccb5cc05a6172dd4f4172043fc1f055b
Rhoda Eboreime <rhodaivioje@gmail.com>
2025-06-05 09:27:51 +0100

    feat: Add Firebase configuration to .env and update firebase.js to use environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,21 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import firebase from "firebase/compat/app";
 import { getFirestore } from "firebase/firestore";
-// Required for side-effects
 import "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "AIzaSyCfvLTXn3K4Bdzt7byDmBOYriSXxs63o-o",
-	authDomain: "peoples-profile.firebaseapp.com",
-	projectId: "peoples-profile",
-	storageBucket: "peoples-profile.appspot.com",
-	messagingSenderId: "68430614685",
-	appId: "1:68430614685:web:56afc33a2a87db2d464474",
-	measurementId: "G-ZFJ9LT03LB",
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_FIREBASE_APP_ID,
+	measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
